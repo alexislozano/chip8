95b54fb1c6af2024e3ed83de97a3ab310602789e
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2019-07-28 23:12:38 +0200

    Working emulator

--- src/chip8.rs
+++ src/chip8.rs
@@ -28,7 +28,7 @@ pub struct Chip8 {
     display: [[bool; 64]; 32],
     delay_timer: u8,
     sound_timer: u8,
-    stack: Vec<u16>,
+    stack: [u16; 16],
     sp: usize,
     keys: [bool; 16],
 }
@@ -44,7 +44,7 @@ impl Chip8 {
             display: [[false; 64]; 32],
             delay_timer: 0,
             sound_timer: 0,
-            stack: vec![],
+            stack: [0; 16],
             sp: 0,
             keys: [false; 16],
         }
@@ -60,10 +60,27 @@ impl Chip8 {
         }
     }
 
+    pub fn set_key(&mut self, key: u8, val: bool) {
+        self.keys[key as usize] = val;
+    }
+
+    pub fn display(&self) -> [[bool; 64]; 32] {
+        self.display
+    }
+
+    pub fn sound_timer(&self) -> u8 {
+        self.sound_timer
+    }
+
     pub fn run_cycle(&mut self) {
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+        if self.sound_timer > 0 {
+            self.sound_timer -= 1;
+        }
         self.opcode = u16::from(self.memory[usize::from(self.pc)]) << 8
             | u16::from(self.memory[usize::from(self.pc + 1)]);
-        println!("{:x?}", self.opcode);
         match (
             self.opcode >> 12,
             (self.opcode >> 8) % 16,
@@ -74,17 +91,18 @@ impl Chip8 {
                 self.display = [[false; 64]; 32];
             }
             (0x0, 0x0, 0xE, 0xE) => {
-                self.pc = self.stack[0];
+                self.pc = self.stack[self.sp];
                 self.sp -= 1;
             }
-            (0x0, n1, n2, n3) => {}
+            (0x0, 0x0, 0x0, 0x0) => {
+            }
             (0x1, n1, n2, n3) => {
-                self.pc = (n1 << 8) + (n2 << 4) + n3;
+                self.pc = (n1 << 8) + (n2 << 4) + n3 - 2;
             }
             (0x2, n1, n2, n3) => {
                 self.sp += 1;
-                self.stack.push(self.pc);
-                self.pc = (n1 << 8) + (n2 << 4) + n3;
+                self.stack[self.sp] = self.pc;
+                self.pc = (n1 << 8) + (n2 << 4) + n3 - 2;
             }
             (0x3, x, k1, k2) => {
                 if self.registers[x as usize] == ((k1 << 4) + k2) as u8 {
@@ -105,7 +123,13 @@ impl Chip8 {
                 self.registers[x as usize] = ((k1 << 4) + k2) as u8;
             }
             (0x7, x, k1, k2) => {
-                self.registers[x as usize] += ((k1 << 4) + k2) as u8;
+                let rx = u16::from(self.registers[x as usize]);
+                let k = (k1 << 4) + k2;
+                self.registers[x as usize] = if rx + k > 0xFF {
+                    (rx + k) % 0x100
+                } else {
+                    rx + k
+                } as u8;
             }
             (0x8, x, y, 0x0) => {
                 self.registers[x as usize] = self.registers[y as usize];
@@ -124,24 +148,25 @@ impl Chip8 {
                 let ry = u16::from(self.registers[y as usize]);
                 self.registers[x as usize] = if rx + ry > 0x100 {
                     self.registers[15] = 1;
-                    (rx + ry - 0x100) as u8
+                    (rx + ry) % 0x100
                 } else {
                     self.registers[15] = 0;
-                    (rx + ry) as u8
-                };
+                    rx + ry
+                } as u8;
             }
             (0x8, x, y, 0x5) => {
-                let rx = self.registers[x as usize];
-                let ry = self.registers[y as usize];
+                let rx = u16::from(self.registers[x as usize]);
+                let ry = u16::from(self.registers[y as usize]);
                 self.registers[x as usize] = if rx > ry {
                     self.registers[15] = 1;
                     rx - ry
                 } else {
                     self.registers[15] = 0;
-                    0
-                }
+                    0x100 - (ry - rx)
+                    
+                } as u8;
             }
-            (0x8, x, y, 0x6) => {
+            (0x8, x, _y, 0x6) => {
                 let rx = self.registers[x as usize];
                 if rx % 2 == 1 {
                     self.registers[15] = 1;
@@ -151,28 +176,28 @@ impl Chip8 {
                 self.registers[x as usize] = rx / 2;
             }
             (0x8, x, y, 0x7) => {
-                let rx = self.registers[x as usize];
-                let ry = self.registers[y as usize];
+                let rx = u16::from(self.registers[x as usize]);
+                let ry = u16::from(self.registers[y as usize]);
                 self.registers[x as usize] = if ry > rx {
                     self.registers[15] = 1;
                     ry - rx
                 } else {
                     self.registers[15] = 0;
-                    0
-                }
+                    0x100 - (rx - ry)
+                } as u8
             }
-            (0x8, x, y, 0xE) => {
-                let rx = self.registers[x as usize];
+            (0x8, x, _y, 0xE) => {
+                let rx = u16::from(self.registers[x as usize]);
                 if rx >> 7 == 1 {
                     self.registers[15] = 1;
                 } else {
                     self.registers[15] = 0;
                 };
-                self.registers[x as usize] = if u16::from(rx) * 2 > 0xFF {
-                    0xFF
+                self.registers[x as usize] = if rx * 2 > 0xFF {
+                    (rx * 2) % 0x100
                 } else {
                     rx * 2
-                };
+                } as u8;
             }
             (0x9, x, y, 0x0) => {
                 if self.registers[x as usize] != self.registers[y as usize] {
@@ -183,13 +208,32 @@ impl Chip8 {
                 self.i = (n1 << 8) + (n2 << 4) + n3;
             }
             (0xB, n1, n2, n3) => {
-                self.pc = (n1 << 8) + (n2 << 4) + n3 + u16::from(self.registers[0]);
+                self.pc = (n1 << 8) + (n2 << 4) + n3 + u16::from(self.registers[0]) - 2;
             }
             (0xC, x, k1, k2) => {
                 let mut rng = rand::thread_rng();
-                self.registers[x as usize] = rng.gen::<u8>() & ((k1 << 4) + k2) as u8;
+                let r = rng.gen::<u8>();
+                self.registers[x as usize] = r & ((k1 << 4) + k2) as u8;
             }
-            (0xD, x, y, n) => {}
+            (0xD, x, y, n) => {
+                let rx = u16::from(self.registers[x as usize]);
+                let ry = u16::from(self.registers[y as usize]);
+                let sprite = &self.memory[self.i as usize..(self.i + n) as usize];
+                let mut collision = false;
+                for h in 0..n {
+                    for w in 0..8 {
+                        let new_w = (rx + w) % 64;
+                        let new_h = (ry + h) % 32;
+                        let old_pixel = self.display[new_h as usize][new_w as usize];
+                        let new_pixel = (sprite[h as usize] << w >> 7) == 1;
+                        self.display[new_h as usize][new_w as usize] = old_pixel ^ new_pixel;
+                        if old_pixel && !self.display[new_h as usize][new_w as usize] {
+                            collision = true;
+                        }
+                    }
+                }
+                self.registers[15] = collision as u8;
+            }
             (0xE, x, 0x9, 0xE) => {
                 if self.keys[self.registers[x as usize] as usize] {
                     self.pc += 2;
@@ -203,14 +247,41 @@ impl Chip8 {
             (0xF, x, 0x0, 0x7) => {
                 self.registers[x as usize] = self.delay_timer;
             }
-            (0xF, x, 0x0, 0xA) => {}
-            (0xF, x, 0x1, 0x5) => {}
-            (0xF, x, 0x1, 0x8) => {}
-            (0xF, x, 0x1, 0xE) => {}
-            (0xF, x, 0x2, 0x9) => {}
-            (0xF, x, 0x3, 0x3) => {}
-            (0xF, x, 0x5, 0x5) => {}
-            (0xF, x, 0x6, 0x5) => {}
+            (0xF, x, 0x0, 0xA) => {
+                match self.keys.iter().position(|k| *k) {
+                    Some(i) => self.registers[x as usize] = i as u8,
+                    None => self.pc -= 2
+                };
+            }
+            (0xF, x, 0x1, 0x5) => {
+                self.delay_timer = self.registers[x as usize];
+            }
+            (0xF, x, 0x1, 0x8) => {
+                self.sound_timer = self.registers[x as usize];
+            }
+            (0xF, x, 0x1, 0xE) => {
+                self.i += u16::from(self.registers[x as usize]);
+            }
+            (0xF, x, 0x2, 0x9) => {
+                self.i = u16::from(self.registers[x as usize]) * 5;
+            }
+            (0xF, x, 0x3, 0x3) => {
+                let rx = self.registers[x as usize];
+                let (k1, k2, k3) = (rx / 100, rx / 10 % 10, rx % 10);
+                self.memory[self.i as usize] = k1;
+                self.memory[(self.i + 1) as usize] = k2;
+                self.memory[(self.i + 2) as usize] = k3;
+            }
+            (0xF, x, 0x5, 0x5) => {
+                self.memory[self.i as usize..(self.i + x + 1) as usize].clone_from_slice(
+                    &self.registers[0..(x + 1) as usize]
+                );
+            }
+            (0xF, x, 0x6, 0x5) => {
+                self.registers[0..(x + 1) as usize].clone_from_slice(
+                    &self.memory[self.i as usize..(self.i + x + 1) as usize]
+                );
+            }
             opcode => {
                 println!("This opcode does not exist : {:?}", opcode);
             }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,172 @@
 mod chip8;
-
 use crate::chip8::Chip8;
-use std::fs::read;
 
-fn main() {
+use std::fs::read;
+use std::{thread, time};
+
+use sdl2::rect::Rect;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::audio::{AudioCallback, AudioSpecDesired, AudioStatus};
+
+use clap::{crate_authors, crate_name, crate_version, App, Arg};
+
+struct SquareWave {
+    phase_inc: f32,
+    phase: f32,
+    volume: f32
+}
+
+impl AudioCallback for SquareWave {
+    type Channel = f32;
+
+    fn callback(&mut self, out: &mut [f32]) {
+        for x in out.iter_mut() {
+            *x = if self.phase >= 0.0 && self.phase < 0.5 {
+                self.volume
+            } else {
+                -self.volume
+            };
+            self.phase = (self.phase + self.phase_inc) % 1.0;
+        }
+    }
+}
+
+fn set_key(chip8: &mut Chip8, keycode: Keycode, keydown: bool) {
+    match keycode {
+        Keycode::Num1 => chip8.set_key(0, keydown),
+        Keycode::Num2 => chip8.set_key(1, keydown),
+        Keycode::Num3 => chip8.set_key(2, keydown),
+        Keycode::Num4 => chip8.set_key(3, keydown),
+        Keycode::Q => chip8.set_key(4, keydown),
+        Keycode::W => chip8.set_key(5, keydown),
+        Keycode::E => chip8.set_key(6, keydown),
+        Keycode::R => chip8.set_key(7, keydown),
+        Keycode::A => chip8.set_key(8, keydown),
+        Keycode::S => chip8.set_key(9, keydown),
+        Keycode::D => chip8.set_key(10, keydown),
+        Keycode::F => chip8.set_key(11, keydown),
+        Keycode::Z => chip8.set_key(12, keydown),
+        Keycode::X => chip8.set_key(13, keydown),
+        Keycode::C => chip8.set_key(14, keydown),
+        Keycode::V => chip8.set_key(15, keydown),
+        _ => ()
+    }
+}
+
+
+fn run(file: Vec<u8>) {
+    let size = 10;
+    let sdl_context = sdl2::init().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem.window("Chip8", 64 * size, 32 * size)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let audio_subsystem = sdl_context.audio().unwrap();
+    let desired_spec = AudioSpecDesired {
+        freq: Some(44100),
+        channels: Some(1),
+        samples: None
+    };
+    let device = audio_subsystem.open_playback(None, &desired_spec, |spec| {
+        SquareWave {
+            phase_inc: 440.0 / spec.freq as f32,
+            phase: 0.0,
+            volume: 0.25
+        }
+    }).unwrap();
+
     let mut chip8 = Chip8::new();
     chip8.load_font();
+    
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
 
-    match read("../c8games/MAZE") {
-        Ok(file) => {
-            chip8.load_game(file);
-            for _ in 0..5 {
-                chip8.run_cycle();
+    chip8.load_game(file);
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                Event::KeyDown { keycode: Some(keycode), ..} => {
+                    set_key(&mut chip8, keycode, true);
+                },
+                Event::KeyUp { keycode: Some(keycode), ..} => {
+                    set_key(&mut chip8, keycode, false);
+                }
+                _ => {}
             }
         }
-        Err(_) => eprintln!("Cannot open file"),
+
+        chip8.run_cycle();
+
+        match device.status() {
+            AudioStatus::Stopped | AudioStatus::Paused => {
+                if chip8.sound_timer() > 0 {
+                    device.resume();
+                }
+            }
+            AudioStatus::Playing => {
+                if chip8.sound_timer() == 0 {
+                    device.pause();
+                }
+            }
+        }
+
+        let pixels = chip8.display();
+        for h in 0..32 {
+            for w in 0..64 {
+                if pixels[h][w] {
+                    canvas.set_draw_color(Color::RGB(255, 255, 255));
+                } else {
+                    canvas.set_draw_color(Color::RGB(0, 0, 0));
+                }
+                canvas.fill_rect(Rect::new(
+                    w as i32 * size as i32, 
+                    h as i32 * size as i32,
+                    size,
+                    size, 
+                )).unwrap();
+            }
+        }
+        canvas.present();
+
+        thread::sleep(time::Duration::from_millis(2));
+    }
+}
+
+
+fn main() {
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("An implementation of a Chip 8 emulator")
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .value_name("FILE")
+                .help("Sets a file to process")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    match matches.value_of("file") {
+        Some(filename) => match read(filename) {
+            Ok(file) => {
+                run(file);
+            }
+            Err(_) => eprintln!("Cannot open file"),
+        },
+        None => eprintln!("Please set the -f flag with a file")
     }
 }
